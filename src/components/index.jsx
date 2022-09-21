f24571f758655574a266b4704984b9beec878908
Bramono Triprastowo <bramonotriprastowo@yahoo.com>
2022-09-21 13:32:08 +0700

    first commit

---
+++ src/components/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Provider } from "react-redux";
+import store from "../app/store";
+import Counter from "./pembahasan/Counter";
+
+const Redux = () => {
+  return (
+    <>
+      <Provider store={store}>
+        <Counter />
+      </Provider>
+    </>
+  );
+};
+
+export default Redux;
